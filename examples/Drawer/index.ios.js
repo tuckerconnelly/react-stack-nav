465d5a8670b95c77c119b9b827a2606e4a1ea507
Tucker Connelly <web@tuckerconnelly.com>
2016-12-06 09:54:46 -0500

    Move examples over to react-stack-nav-examples repo

--- examples/Drawer/index.ios.js
+++
@@ -1,8 +0,0 @@
-import { AppRegistry } from 'react-native'
-
-import App from './src/index'
-
-// NOTE For Uranium/carbon-ui
-global.matchMedia = () => ({ addListener: () => 0, removeListener: () => 0 })
-
-AppRegistry.registerComponent('Drawer', () => App)
--- examples/Drawer/src/modules/Index/HomePage.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import { Headline } from 'carbon-ui'
-
-export default () => <Headline>Home</Headline>
